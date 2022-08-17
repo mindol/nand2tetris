b7cc31b6658e26bcc5a2faa8a3c7e03ac4b535d2
mindol <mindol@kaist.ac.kr>
2022-08-17 17:15:36 +0900

    Implement StackArithmetic part

--- projects/07/VMtranslator.py
+++ projects/07/VMtranslator.py
@@ -60,9 +60,147 @@ class Parser:
     def arg2(self):
         ctype = self.commandType()
         if ctype in [Ctype.C_PUSH, Ctype.C_POP, Ctype.C_FUNCTION, Ctype.C_CALL]:
-            return self.current[2]
+            return int(self.current[2])
         else:
             ERROR("[Parser::arg2] Called with wrong command type")
+
+class CodeWriter:
+    def __init__(self):
+        self.file = None
+        self.label_count = 0
+
+    def setFileName(self, filename):
+        self.file = open(filename, 'w')
+
+    def writeArithmetic(self, command):
+        res = ""
+        if command == "add": # 5 instructions
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "M=D+M",
+            ))
+        elif command == "sub": # 5 instructions
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "M=M-D",
+            ))
+        elif command == "neg": # 3 instructions
+            res = "\n".join((
+                "@SP",
+                "A=M-1",
+                "M=-M",
+            ))
+        elif command == "eq": # 8 insts if x=y, 11 insts otherwise
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "MD=M-D",
+                "M=!M",
+                "@EQ_{}".format(self.label_count),
+                "D;JEQ",
+                "@SP",
+                "A=M-1",
+                "M=0",
+                "(EQ_{})".format(self.label_count),
+            ))
+            self.label_count += 1
+        elif command == "gt": # 10 insts if x=y, 12 insts otherwise
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "D=M-D",
+                "@GT_{}".format(self.label_count),
+                "D;JGT",
+                "@SP",
+                "A=M-1",
+                "M=0",
+                "@ENDGT_{}".format(self.label_count),
+                "0;JMP",
+                "(GT_{})".format(self.label_count),
+                "@SP",
+                "A=M-1",
+                "M=-1",
+                "(ENDGT_{})".format(self.label_count),
+            ))
+            self.label_count += 1
+        elif command == "lt": # 10 insts if x=y, 12 insts otherwise
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "D=M-D",
+                "@LT_{}".format(self.label_count),
+                "D;JLT",
+                "@SP",
+                "A=M-1",
+                "M=0",
+                "@ENDLT_{}".format(self.label_count),
+                "0;JMP",
+                "(LT_{})".format(self.label_count),
+                "@SP",
+                "A=M-1",
+                "M=-1",
+                "(ENDLT_{})".format(self.label_count),
+            ))
+            self.label_count += 1
+        elif command == "and": # 5 instructions
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "M=D&M",
+            ))
+        elif command == "or": # 5 instructions
+            res = "\n".join((
+                "@SP",
+                "AM=M-1",
+                "D=M",
+                "A=A-1",
+                "M=D|M",
+            ))
+        elif command == "not": # 3 instructions
+            res = "\n".join((
+                "@SP",
+                "A=M-1",
+                "M=!M",
+            ))
+        else:
+            ERROR("[writeArithmetic] Unknown token:", command)
+        
+        self.file.write(res + "\n")
+
+    def writePushPop(self, command, segment, index):
+        res = ""
+        if segment == "constant": # 6 instructions constant push
+            res = "\n".join((
+                "@{}".format(index),
+                "D=A",
+                "@SP",
+                "AM=M+1",
+                "A=A-1",
+                "M=D"
+            ))
+        else:
+            ERROR("[writePushPop] Unknown token:", segment)
+
+        self.file.write(res + "\n")
+
+    def __del__(self):
+        if self.file is not None:
+            self.file.close()
+            self.file = None
 
 def main():
     if len(sys.argv) < 2:
@@ -73,8 +211,18 @@ def main():
 
     print(arg)
     p = Parser(arg)
+
+    path = arg.split("/")
+    path[-1] = path[-1].replace(".vm", ".asm")
+    print("/".join(path))
+    cw = CodeWriter()
+    cw.setFileName("/".join(path))
+    
     while p.hasMoreCommands():
         p.advance()
-        print(p.current, p.commandType())
+        if p.commandType() == Ctype.C_ARITHMETIC:
+            cw.writeArithmetic(p.current[0])
+        elif p.commandType() == Ctype.C_PUSH:
+            cw.writePushPop(p.commandType(), p.arg1(), p.arg2())
 
 main()
